f888f6f8f52bdac1fffb9e79ad1e89813a0f630d
xfusek08 <xfusek08@stud.fit.vutbr.cz>
2019-02-26 12:11:34 +0100

    Setup base build system script files.
    Contains CmakeLists.txt and premake5.lua.
    Premake script is just experimental and not working properly.

---
+++ premake5.lua
@@ -0,0 +1,9 @@
+
+workspace "GPUTerrainProject"
+	architecture "x64"
+	startproject "GPUTerrainDemo"
+
+basedir ("D:/MyProjects/GPUEngine_all/_build/GPUTerrainProject")
+
+include "GPUTerrainDemo"
+include "libs/GPUTerrain"
